163ae2f34c53e1525684e4128be30507ecc06bd1
Alereza_RaDBlaCK01 <aliireza01ahmadi@gmail.com>
2023-12-19 02:43:50 +0100

    Adding end part

--- js/game.js
+++ js/game.js
@@ -6,6 +6,7 @@ const questionText = document.getElementById("question-text");
 const answerList = document.querySelectorAll(".answer-text");
 const scoreText = document.getElementById("score");
 const nextButton = document.getElementById("next-button");
+const finishButton = document.getElementById("finish-button");
 const questionNumber = document.getElementById("question-number");
 
 const COREECT_BONUS = 10;
@@ -63,8 +64,7 @@ const nextHandler = () => {
         removeClasses()
         showQeustion();
     } else {
-        window.location.assign("/end.html")
-
+        finishHandler()
     }
 }
 
@@ -72,8 +72,14 @@ const removeClasses = () => {
     answerList.forEach((button) => (button.className = "answer-text"));
 };
 
+const finishHandler = () => {
+    localStorage.setItem("score", JSON.stringify(score))
+    window.location.assign("/end.html")
+}
+
 window.addEventListener("load", fetchData);
 nextButton.addEventListener("click", nextHandler)
+finishButton.addEventListener("click", finishHandler)
 answerList.forEach((button, index) => {
     button.addEventListener("click", (event) => checkAnswer(event, index));
 })
